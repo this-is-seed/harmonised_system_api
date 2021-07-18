02302c19b9735d1977614091c077e89e79506569
Dishant Gupta <dishant.guptaaa@gmail.com>
2021-07-18 21:48:59 +0530

    changed get route to show for product detail

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     resources :products, only: [:index, :update]
-    get 'get_product_description/:id' => 'products#show'
+    show 'get_product_description/:id' => 'products#show'
   end
 end
